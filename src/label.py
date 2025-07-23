daf584e6fcc3b12ec3ee2debc0d0737b41d0dd1a
SomdattaNag <nagsomdatta2@gmail.com>
2025-07-23 09:41:50 +0530

    Fix Streamlit deployment path

--- src/label.py
+++ src/label.py
@@ -1,17 +1,20 @@
 import os
 import pandas as pd
-data=[]
-hairtype=['CurlyHair','KinkyHair', 'StraightHair', 'WavyHair']
 
-if __name__=="__main__":
+data = []
+hairtype = ['CurlyHair', 'KinkyHair', 'StraightHair', 'WavyHair']
+
+if __name__ == "__main__":
     for type in hairtype:
-        folder_path= f"../Dataset/{type}"
+        folder_path = os.path.join("Dataset", type)
         for img_name in os.listdir(folder_path):
-            img_path = f"{folder_path}/{img_name}"
+            img_path = os.path.join(folder_path, img_name)
             data.append([img_path, type])
+
     df = pd.DataFrame(data, columns=["Image Path", "Label"])
 
-    #saved as csv
-    output_dir = "../csv_datasets"
-    os.makedirs(output_dir,exist_ok=True)
-    df.to_csv(f"{output_dir}/hair_dataset.csv", index=False)
+    #save as csv
+    output_dir = "csv_datasets"
+    os.makedirs(output_dir, exist_ok=True)
+    df.to_csv(os.path.join(output_dir, "hair_dataset.csv"), index=False)
+
